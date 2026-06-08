f14f0e56ee6a4a6541bcd5e56eb4533107c68807
thehemanuj <ayushkr.shukla2004@gmail.com>
2026-06-08 19:39:07 +0530

    new : ref and value property in spring

--- SpringDemo/src/main/java/org/ayushshukla/Alien.java
+++ SpringDemo/src/main/java/org/ayushshukla/Alien.java
@@ -1,8 +1,17 @@
 package org.ayushshukla;
 
 public class Alien {
+    public Laptop getLaptop() {
+        return laptop;
+    }
 
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    Laptop laptop;
     public void code(){
+        laptop.boot();
         System.out.println("I am coding");
     }
 }
---
+++ SpringDemo/src/main/java/org/ayushshukla/Laptop.java
@@ -0,0 +1,7 @@
+package org.ayushshukla;
+
+public class Laptop {
+    public void boot(){
+        System.out.println("Booting laptop");
+    }
+}
